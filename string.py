4e2935d593f30a4a24336d8493c659a6fe767a70
SandraKTI2025 <sandra.savinski@tktk.ee>
2026-05-18 00:53:31 +0300

    Create string.py

---
+++ string.py
@@ -0,0 +1,12 @@
+text = "Hello"
+
+#for i in text:
+#print(i)
+
+#sone tagurpidi, iga taht uhel real
+
+lenght = len(text)
+print(lenght)
+
+for letter in range(len(text) -1, -1, -1):
+    print(text[letter])
